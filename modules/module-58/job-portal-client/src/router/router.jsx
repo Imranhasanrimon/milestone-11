1a6b10c3b6e7cf9d1aad3576497b0f1767eaee8b
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-11 15:55:12 +0600

    adding homepage component

--- modules/module-58/job-portal-client/src/router/router.jsx
+++ modules/module-58/job-portal-client/src/router/router.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import HomePage from "../pages/HomePage";
+import MainLayout from "../layout/MainLayout";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage></HomePage>
+            }
+        ]
     },
 ]);
 
