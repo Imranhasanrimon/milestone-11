1cbfc96b44b1f800c1d89c0b4b271f7c42be2575
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-12 06:33:29 +0600

    adding banner component

--- modules/module-58/job-portal-client/src/pages/HomePage.jsx
+++ modules/module-58/job-portal-client/src/pages/HomePage.jsx
@@ -1,8 +1,9 @@
+import Banner from "../components/Banner";
 
 const HomePage = () => {
     return (
         <div>
-            this is home page
+            <Banner></Banner>
         </div>
     );
 };
