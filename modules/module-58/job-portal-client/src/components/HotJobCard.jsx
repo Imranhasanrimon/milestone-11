35b8acdb4c28fab6d8cd6b4cb8904cd2fd44ddda
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-12 15:29:56 +0600

    card design almost done

--- modules/module-58/job-portal-client/src/components/HotJobCard.jsx
+++ modules/module-58/job-portal-client/src/components/HotJobCard.jsx
@@ -1,9 +1,32 @@
+import { IoLocationSharp } from "react-icons/io5";
 
 const HotJobCard = ({ job }) => {
-    console.log(job);
+    const { title, location, jobType, category, applicationDeadline, salaryRange, description, company, company_logo, requirements } = job
     return (
-        <div>
-
+        <div className="card card-compact bg-base-100  shadow-xl">
+            <div className="flex items-center gap-4 pt-3">
+                <figure>
+                    <img
+                        className="w-16"
+                        src={company_logo}
+                        alt="Shoes" />
+                </figure>
+                <div>
+                    <h2 className="text-2xl font-semibold">{company}</h2>
+                    <p className="flex items-center gap-1"><IoLocationSharp />{location}</p>
+                </div>
+            </div>
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="flex gap-5 flex-wrap">
+                    {requirements.map((requirement, i) => <p className="border rounded-lg px-2 py-1" key={i}>{requirement}</p>)}
+                </div>
+                <div className="card-actions justify-end items-center">
+                    <p>Salary: ${salaryRange.min} - ${salaryRange.max}</p>
+                    <button className="btn btn-primary btn-sm">Apply</button>
+                </div>
+            </div>
         </div>
     );
 };
