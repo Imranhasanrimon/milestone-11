a195b6fcaddfae601d9b065d2d54b0063276212a
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-14 14:39:46 +0600

    adding job requirement

--- modules/module-58/job-portal-client/src/pages/AdJob.jsx
+++ modules/module-58/job-portal-client/src/pages/AdJob.jsx
@@ -4,17 +4,31 @@ const AdJob = () => {
         <div>
             thsi is add job page for ading job by reqruiter.
             <form className="card-body">
+                {/* job title */}
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Job Title</span>
                     </label>
                     <input type="text" name="title" placeholder="Job Title" className="input input-bordered" required />
                 </div>
+                {/* job location */}
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text">Job Location</span>
                     </label>
                     <input type="text" name="locations" placeholder="Job Location" className="input input-bordered" required />
+                </div>
+                {/* job location */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Location</span>
+                    </label>
+                    <select className="select input-bordered select-ghost w-full ">
+                        <option disabled selected>Pick the best JS framework</option>
+                        <option>Svelte</option>
+                        <option>Vue</option>
+                        <option>React</option>
+                    </select>
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn btn-primary">Add Job</button>
