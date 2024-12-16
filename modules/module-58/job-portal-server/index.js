b7f3f34bcf2e9f9016ac40062d5292b33d623aa4
Imranhasanrimon <imranhasanrimon5@gmail.com>
2024-12-16 19:18:20 +0600

    adding logger middleware function

--- modules/module-58/job-portal-server/index.js
+++ modules/module-58/job-portal-server/index.js
@@ -18,7 +18,10 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-
+const logger = (req, res, next) => {
+    console.log('inside the logger middleware');
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7hbnv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
